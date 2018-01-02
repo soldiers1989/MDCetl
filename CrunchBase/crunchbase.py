87e7422354883e4be4b76f50f01d89b1952a60a0
Mussie Nadew <muchichi@gmail.com>
2018-01-02 12:00:01 -0500

    Save all crunchbase data for the 4 main nodes of the API.

--- CrunchBase/crunchbase.py
+++ CrunchBase/crunchbase.py
@@ -1,9 +1,11 @@
-
+import os
 import pandas as pd
+import time
 
 from Shared.common import Common as CM
 from Shared.enums import VAR, CONSTANTS
 from Shared.file_service import FileService
+from Shared.db import DB as db
 
 
 class Crunchbase:
@@ -17,7 +19,12 @@ class Crunchbase:
 		self.url_cat = CM.get_config('config.ini', 'crunch_base', 'url_cat') + self.api_tokens
 		self.url_loc = CM.get_config('config.ini', 'crunch_base', 'url_loc') + self.api_token
 
-		self.path = CM.get_config('config.ini', 'box_file_path', 'path_data')
+		self.path = CM.get_config('config.ini', 'box_file_path', 'path_crunchbase')
+
+		self.org_summary = CM.get_config('config_sql.ini', 'db_sql_crunchbase', 'sql_organizations_insert')
+		self.people = CM.get_config('config_sql.ini', 'db_sql_crunchbase', 'sql_people_insert')
+		self.category = CM.get_config('config_sql.ini', 'db_sql_crunchbase', 'sql_category_insert')
+		self.location = CM.get_config('config_sql.ini', 'db_sql_crunchbase', 'sql_location_insert')
 
 		self.data = None
 		self.file_name = 'CB_{}_{}.csv'
@@ -41,6 +48,8 @@ class Crunchbase:
 			total_items = self.data.json()[VAR.data.value][VAR.paging.value][VAR.total_items.value]
 			number_of_pages = self.data.json()[VAR.data.value][VAR.paging.value][VAR.number_of_pages.value]
 			cols = self.data.json()[VAR.data.value][VAR.items.value][0][VAR.properties.value].keys()
+			cols = list(cols)
+			cols.append('uuid')
 			print(cols)
 			print('Total items: {}\nTotal Pages: {}'.format(total_items, number_of_pages))
 			data_list = []
@@ -53,12 +62,24 @@ class Crunchbase:
 					dt[VAR.uuid.value] = data[i][VAR.uuid.value]
 					data_list.append(dt)
 			df = pd.DataFrame(data_list, columns=cols)
-			#df.to_csv(self.file_name.format(object_name, str(time.time())), sep=',', columns=cols, index=False)
-			#print('File saved successfully!')
+			df.to_csv(self.file_name.format(object_name, str(time.time())), sep=',', columns=cols, index=False)
+			print('File saved successfully!')
 		else:
 			print('SNAP! Something goes wrong.\nSTATUS: {}\nMESSAGE: {}'.format(self.data.json()[0]['status'], self.data.json()[0]['message']))
+
+	def save_cb_data(self):
+		try:
+			print(os.getcwd(), self.path)
+			#df = pd.read_csv('CB_organization_summary_1513788334.csv')
+			#df = pd.read_csv('CB_people_summary_1513794895.csv')
+			#df = pd.read_csv('CB_CATEGORIES_1514474648.46174.csv')
+			df = pd.read_csv('CB_LOCATIONS_1514478442.280132.csv')
+			print('{}\n{}'.format(len(df), df.columns))
+			db.save_data_chunk(df, self.location, chunk_size=5000)
+		except FileNotFoundError as f:
+			print(f)
 
 
 if __name__ == '__main__':
 	crb = Crunchbase()
-	crb.get_organizations()
+	crb.save_cb_data()

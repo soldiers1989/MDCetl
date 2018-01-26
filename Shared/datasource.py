749ddb300009ee40263502cff6e915e2413fb5a4
Mussie Nadew <muchichi@gmail.com>
2018-01-26 11:25:15 -0500

    Initializing File service and other properties only when needed.

--- Shared/datasource.py
+++ Shared/datasource.py
@@ -1,15 +1,13 @@
 import pandas as pd
 from Shared import db, enums, common, file_service, batch
 import datetime
-# https://www.scottbrady91.com/Email-Verification/Python-Email-Verification-Script
-# https://pypi.python.org/pypi/validate_email
 
 
 class DataSource:
 
 	desired_width = 820
 
-	def __init__(self, header, item):
+	def __init__(self, header, item, datasource:enums.MDCDataSource):
 		pd.set_option('display.width', self.desired_width)
 		self.common = common.Common
 		_y, _q = self.common.fiscal_year_quarter(datetime.datetime.utcnow())
@@ -18,10 +16,12 @@ class DataSource:
 		self.data = None
 		self.db = db.DB()
 		self.batch = batch.BatchService()
-		self.enum = enums
 
 		if header is not '' and item is not '':
 			self.path = self.common.get_config('config.ini', header, item)
 			self.file = file_service.FileService(self.path)
 
-		self.cb_sql_insert = self.common.get_config('config_sql.ini', 'db_sql_cbinsights', 'sql_cbinsights_insert')
+		if datasource.value == enums.MDCDataSource.CBINSIGHT.value:
+			self.cb_sql_insert = self.common.get_config('config_sql.ini', header, item)
+		elif datasource.value == enums.MDCDataSource.OSVP.value:
+			self.osvp_sql_insert = self.common.get_config('config_sql.ini', header, item)

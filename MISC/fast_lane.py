4d0fca51ab96d3552142bd8c248c995954f82367
Mussie Nadew <muchichi@gmail.com>
2018-02-05 16:56:37 -0500

    Miscellaneous changes.

--- MISC/fast_lane.py
+++ MISC/fast_lane.py
@@ -1,5 +1,6 @@
 import Shared.datasource as ds
 import Shared.enums as enum
+from Shared.file_service import FileService
 
 
 class FastLane(ds.DataSource):
@@ -7,7 +8,7 @@ class FastLane(ds.DataSource):
 		super().__init__('box_file_path', 'path_other', enum.DataSourceType.DATA_CATALYST)
 
 	def read_misc_file(self):
-		self.data = self.file.read_source_file(enum.FileType.SPREAD_SHEET, enum.MDCDataSource.OTHER, enum.Combine.FOR_NONE.value)
+		self.data = self.file.read_source_file(enum.FileType.SPREAD_SHEET.value, enum.MDCDataSource.OTHER, enum.Combine.FOR_NONE.value)
 		# print(self.data.head())
 
 	def push_data_to_db(self):
@@ -17,7 +18,16 @@ class FastLane(ds.DataSource):
 		self.db.bulk_insert(self.enum.SQL.sql_other_insert.value, values=values)
 		print(values[0])
 
+	def last_minute_issue(self):
+		path = '/Users/mnadew/Box Sync/WorkBench/BAP/BAP_FY18/FY18_Q3/LMI'
+		fs = FileService(path)
+		_, _, data = fs.read_source_file(enum.FileType.SPREAD_SHEET.value, enum.MDCDataSource.BAP, enum.Combine.FOR_NONE.value)
+		# print(data.head())
+		for _, row in data.iterrows():
+			update = 'UPDATE BAP.QuarterlyCompanyData SET  Stage = \'{}\' WHERE [Company Name] = \'{}\''.format(row['Stage'], self.common.sql_friendly(row['Company Name']))
+			print(update)
+
 
 if __name__ == '__main__':
 	fl = FastLane()
-	fl.push_data_to_db()
+	fl.last_minute_issue()

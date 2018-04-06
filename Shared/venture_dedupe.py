7f90d85783684c887eca4ba496629212251079df
Mussie Nadew <muchichi@gmail.com>
2018-04-06 11:58:12 -0400

    Venture dedupe object created.

---
+++ Shared/venture_dedupe.py
@@ -0,0 +1,13 @@
+class VentureDedupe:
+
+    def __init__(self):
+        pass
+
+    def list_possible_duplicates(self):
+        pass
+
+    def consolidate_ventures(self):
+        pass
+
+    def merge_ventures_and_consolidate(self):
+        pass

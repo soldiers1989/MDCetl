b41f8e7e70c8765d22833dfbee9428e59647228d
Mussie Nadew <muchichi@gmail.com>
2018-07-25 13:59:17 -0400

    AWS RDS instance back up script.

--- Shared/aws_scripts.sql
+++ Shared/aws_scripts.sql
@@ -1,3 +1,4 @@
+--Restore SQL Server back up files to AMAZON RDS instances.
 EXEC msdb.dbo.rds_restore_database
      @restore_db_name = 'MDCRaw',
      @S3_arn_to_restore_from = 'arn:aws:s3:::mars-mdc-database-backup/MDCRaw_backup_2018_06_28_010011_4826757.bak'
